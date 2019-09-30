663af5612f4a91c69fffb98559fbc66569c68dc3
robarcand <robbyarcand@aol.com>
2019-09-30 15:46:48 -0400

    changing drivers

--- src/api.js
+++ src/api.js
@@ -1,9 +1,33 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/robarcand';
 
-const getItems() {
-  return Promise.resolve('A successful response!');
+const getItems = function() {
+  //return Promise.resolve('A successful response!');
+  return fetch(`${BASE_URL}/items`);
+  
 }
 
+function createItem(name) {
+ const newItem = JSON.stringify({
+   name: name,
+ })
+
+ return fetch(`${BASE_URL}/items`, {
+   method: 'POST',
+   headers: {'Content-Type': 'application/json'},
+   body: newItem
+    });
+};
+
+function updateItem(id, updateData) {
+  return fetch(`${BASE_URL}/items/${id}`, {
+    method: 'PATCH',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(updateData)
+  });
+};
+
 export default {
-  getItems
+  getItems,
+  createItem,
+  updateItem
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import $ from 'jquery';
+import api from './api';
 
 import 'normalize.css';
 import './index.css';
 
+import store from './store';
 import shoppingList from './shopping-list';
 
 const main = function () {
+  // api.getItems()
+  //   .then(res => res.json())
+  //   .then((items) => {
+  //     const item = items[0];
+  //     return api.updateItem(item.id, { name: 'foobar' });
+  //   })
+  //   .then(res => res.json())
+  //   .then(() => console.log('updated!'));
+    
+
+  // const item = store.items[0];
+  // console.log('current name: ' + item.name);
+  // store.findAndUpdate(item.id, { name: 'foobar' });
+  // console.log('new name: ' + item.name);
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
