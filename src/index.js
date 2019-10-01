ac8de36f5d780160ee2e8945713d2968f58f2ca6
Rob <robbyarcand@aol.com>
2019-10-01 09:32:05 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,62 @@
-/* eslint-disable no-console */
 import $ from 'jquery';
+
 import api from './api';
 
 import 'normalize.css';
 import './index.css';
 
-import store from './store';
 import shoppingList from './shopping-list';
+import store from './store';
 
 const main = function () {
-  api.getItems()
-    .then(res => res.json())
-    .then((items) => {
-      items.forEach((item) => store.addItem(item));
-      shoppingList.render();
-    });
+  // api.getItems()
+  // .then(res => res.json())
+  // .then(res => console.log(res));
+
+  // api.createItem('pears')
+  // .then(res => res.json())
+  // .then((newItem) => {
+  //   return api.getItems();
+  // })
+  // .then(res => res.json())
+  // .then((items) => {
+  //   console.log(items);
+  // });
+
+  // api.getItems()
+  // .then(res => res.json())
+  // .then((items) => {
+  //   items.forEach((item) => store.addItem(item));
+  //   shoppingList.render();
+  // });
+
+  // api.getItems()
+  //   .then(res => res.json())
+  //   .then((items) => {
+  //     const item = items[0];
+  //     return api.updateItem(item.id, { name: 'foobar' });
+  //   })
+  //   .then(res => res.json())
+  //   .then(() => console.log('updated!'));
+
+  // api.getItems()
+  //   .then(res => res.json())
+  //   .then((items) => {
+  //     const item = store.items[0];
+  //     console.log('current name: ' + item.name);
+  //     store.findAndUpdate(item.id, { name: 'foobar' });
+  //     console.log('new name: ' + item.name);
+  //   })
+
+  // api.getItems()
+  //   .then(items => {
+  //     items.forEach(item => store.addItem(item));
+  //     shoppingList.render();
+  //   })
+  //   .catch(err => {
+  //     store.setError(err.message);
+  //     shoppingList.render();
+  //   });
 
   shoppingList.bindEventListeners();
   shoppingList.render();
