4c838b39bbc6cfbe71b6bc4e91ac10212f54a739
Rob <robbyarcand@aol.com>
2019-10-01 09:34:35 -0400

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,23 +1,43 @@
-/* eslint-disable no-console */
-//import item from './item';
-
 const items = [];
 let hideCheckeditems = false;
+let error = null;
 
 const findById = function (id) {
   return this.items.find(currentItem => currentItem.id === id);
 };
 
 const addItem = function (item) {
+  // try {
+  //   item.validateName(name);
+  //   this.items.push(item.create(name));
+  // } catch (e) {
+  //   console.log(e.message);
+  // }
   this.items.push(item);
+
 };
 
-const findAndUpdate = function (id, newData) {
-  const newId = this.items.find(currentItem => currentItem.id === id);
+// const findAndToggleChecked = function (id) {
+//   const currentItem = this.findById(id);
+//   currentItem.checked = !currentItem.checked;
+// };
+
+// const findAndUpdateName = function (id, name) {
+//   try {
+//     item.validateName(name);
+//     const currentItem = this.findById(id);
+//     currentItem.name = name;
+//   } catch (e) {
+//     console.log('Cannot update name: ' + e.message);
+//   }
+// };
+
+const findAndUpdate = function(id, newData) {
+  const newId = this.findById(id);
   Object.assign(newId, newData);
-};
+}
 
-const findAndDelete = function (id) {
+const findAndDelete = function(id) {
   this.items = this.items.filter(currentItem => currentItem.id !== id);
 };
 
@@ -25,12 +45,20 @@ const toggleCheckedFilter = function () {
   this.hideCheckedItems = !this.hideCheckedItems;
 };
 
+const resetError = function () {
+  this.error = null;
+}
+
 export default {
   items,
   hideCheckeditems,
   findById,
   addItem,
+  // findAndToggleChecked,
+  // findAndUpdateName,
   findAndUpdate,
   findAndDelete,
-  toggleCheckedFilter
+  toggleCheckedFilter,
+  error,
+  resetError
 };
